2455cdcd97957209ad51677544db8b7c98953e70
Stacy Betts <stacyo23@yahoo.com>
2020-12-13 14:11:38 -0600

    added app.get to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,27 @@
 const path = require('path'); 
 const fs = require("fs"); 
+
+module.exports = function(app) {
+    //reads api data
+        app.get("/api/notes", function(req, res) {
+    //reads all input data
+            fs.readFile(path.join(__dirname, "..db/db.json"), "utf-8", function(err, data) {
+                if (err) throw err;
+    
+                res.json(data);
+               console.log(data);  
+            })
+            //provides a "good" status
+            return res.status(200); 
+          });
+    
+        //   app.post("/api/notes", function (req, res) {
+    
+            
+        //   }); 
+    
+        //   app.delete("/api/notes/:id", function (req, res) {
+    
+            
+        // }); 
+    }
