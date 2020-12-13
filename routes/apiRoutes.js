98c538499772d04201ff2d28ff7295f52fe62868
Stacy Betts <stacyo23@yahoo.com>
2020-12-13 15:25:44 -0600

    added POST and DELETE modules

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,20 +8,69 @@ module.exports = function(app) {
             fs.readFile(path.join(__dirname, "..db/db.json"), "utf-8", function(err, data) {
                 if (err) throw err;
     
-                res.json(data);
+                res.json(JSON.parse(data));
                console.log(data);  
             })
             //provides a "good" status
             return res.status(200); 
           });
     
+          //create a new entry
           app.post("/api/notes", function (req, res) {
     
-            
+            fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(err, data) {
+                if (err) throw err;
+                const allNotes = JSON.parse(data);
+                console.log(allNotes);
+                const newNotes = [];
+        
+                allNotes.push(req.body);
+                console.log(allNotes)
+        
+                for(let i = 0; i < allNotes.length; i++) {
+        
+                    const addNote = {
+                        title: allNotes[i].title,
+                        text: allNotes[i].text,
+                        id: newNotes.length
+                    };
+        
+                    newNotes.push(addNote);
+                    console.log(newNotes);
+                }
+
+                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(newNotes, null, 2), function(err) {
+                    if (err) throw err;
+                    res.json(req.body);
+                    console.log(newNotes);
+                    console.log(allNotes)
+                });
+                return res.status(200)
+            });
+
           }); 
     
-        //   app.delete("/api/notes/:id", function (req, res) {
-    
+          app.delete("/api/notes/:id", function(req, res) {
+
+            fs.readFile(path.join(__dirname, "../db/db.json"),"utf8", function(err, data) {
+                if (err) throw err;
+        
+                
+                let allNotes = JSON.parse(data);
+                console.log(JSON.stringify(allNotes) + "in delete section now");
+        
+                allNotes = allNotes.filter(function(data) {
+                    return data.id !=req.params.id;
+                });
+        
             
-        // }); 
+                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(allNotes, null, 2), function(err) {
+                    if (err) throw err;
+                    res.json(req.body);
+                    });
+                
+                    
+                });
+                return res.status(200)
+            });
     }
