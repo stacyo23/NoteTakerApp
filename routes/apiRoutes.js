f0baeabe2b87d92a1958d70d8ea59dcfbcee0769
Stacy Betts <stacyo23@yahoo.com>
2020-12-13 16:10:39 -0600

    fixed small syntax error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     //reads api data
         app.get("/api/notes", function(req, res) {
     //reads all input data
-            fs.readFile(path.join(__dirname, "..db/db.json"), "utf-8", function(err, data) {
+            fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", function(err, data) {
                 if (err) throw err;
     
                 res.json(JSON.parse(data));
@@ -20,13 +20,16 @@ module.exports = function(app) {
     
             fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function(err, data) {
                 if (err) throw err;
+               //parse notes into readable format
                 const allNotes = JSON.parse(data);
                 console.log(allNotes);
                 const newNotes = [];
         
+                //adds current note to allNotes
                 allNotes.push(req.body);
                 console.log(allNotes)
         
+                //pushes added note to newNotes array
                 for(let i = 0; i < allNotes.length; i++) {
         
                     const addNote = {
@@ -38,7 +41,7 @@ module.exports = function(app) {
                     newNotes.push(addNote);
                     console.log(newNotes);
                 }
-
+                //writes note
                 fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(newNotes, null, 2), function(err) {
                     if (err) throw err;
                     res.json(req.body);
@@ -57,7 +60,7 @@ module.exports = function(app) {
         
                 
                 let allNotes = JSON.parse(data);
-                console.log(JSON.stringify(allNotes) + "in delete section now");
+                console.log(JSON.stringify(allNotes) + "will be deleted");
         
                 allNotes = allNotes.filter(function(data) {
                     return data.id !=req.params.id;
